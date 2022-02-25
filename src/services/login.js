a4fd41d94714612bb8987a36ed3d8e0fc3ecf02a
Manuel Luna <manuellunada@gmail.com>
2022-02-24 18:27:42 -0600

    token expiration

--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 
+const ONE_WEEK = "604800s";
 exports.generateJwt = (user) => {
-  return jwt.sign(user, process.env.JWT_SECRET);
+  return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: ONE_WEEK });
 };
